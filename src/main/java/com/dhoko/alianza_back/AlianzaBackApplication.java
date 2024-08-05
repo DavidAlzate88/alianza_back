a99b5623f7ae4b6e2786208c61725d8f21629878
Juan Alzate <davids20xx@outlook.com>
2024-08-04 20:21:31 -0500

    first commit

---
+++ src/main/java/com/dhoko/alianza_back/AlianzaBackApplication.java
@@ -0,0 +1,13 @@
+package com.dhoko.alianza_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlianzaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlianzaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhoko/alianza_back/AlianzaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhoko.alianza_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlianzaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
